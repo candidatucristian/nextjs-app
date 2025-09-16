ba0ccce9a7452416069bedbeda0c5a995033690b
Cristian C. <cristiancandidatu@hotmail.com>
2025-09-16 18:25:49 +0300

    routing next

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  fastRefresh: false,
+  // fastRefresh: false,
 };
 
 export default nextConfig;
