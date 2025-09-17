33f51289acd00206a542b39f1815a03077bf59a5
Cristian C. <cristiancandidatu@hotmail.com>
2025-09-17 14:48:00 +0300

    remove things

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Image from "next/image";
 const page = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Image alt="nextjslogo" src="./next.svg" width={180} height={100} />
     </div>
   );
 };
